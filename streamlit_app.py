16694a620e9048428a76707ccd1f08cc059f4988
mjocampo20 <150635904+mjocampo20@users.noreply.github.com>
2023-11-13 09:54:33 +0800

    Update streamlit_app.py
    typo error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
 streamlit.header ('Breakfast Menu')
-Streamlit.title ('Omega 3 & Blueberry Oatmeal')
-Streamlit.title ('kale,Spinach & Rocket Smoothie')
-Streamlit.title ('Hard-Boiled Free-Range Egg')
+streamlit.title ('Omega 3 & Blueberry Oatmeal')
+streamlit.title ('kale,Spinach & Rocket Smoothie')
+streamlit.title ('Hard-Boiled Free-Range Egg')

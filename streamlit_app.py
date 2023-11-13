af0dbfc47ef804da223f938bb3f7272d2f08e836
mjocampo20 <150635904+mjocampo20@users.noreply.github.com>
2023-11-13 09:52:26 +0800

    Update streamlit_app.py
    add other info

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header ('Breakfast Menu')
+Streamlit.title ('Omega 3 & Blueberry Oatmeal')
+Streamlit.title ('kale,Spinach & Rocket Smoothie')
+Streamlit.title ('Hard-Boiled Free-Range Egg')

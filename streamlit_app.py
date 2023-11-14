bb2527600b218328ecb918e7380634d322629529
mjocampo20 <150635904+mjocampo20@users.noreply.github.com>
2023-11-14 13:43:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,9 @@ try:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruity_response.json())
     streamlit.dataframe(fruityvice_normalized)
-streamlit.write('The user entered ', fruit_choice)
+except URLerror as e:
+  streamlit.error()
+
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
